5046f1fa48ab063451604e4adb3aa4b8346131d5
Flip451 <flipflap451@gmail.com>
2023-09-14 23:09:21 +0900

    feat: add loggin feature

--- projects/hello_world_axum/src/main.rs
+++ projects/hello_world_axum/src/main.rs
@@ -1,20 +1,28 @@
-use std::net::SocketAddr;
+use std::{env, net::SocketAddr};
 
-use axum::{Router, routing::get};
 use anyhow::Result;
+use axum::{routing::get, Router};
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
+    // logging の初期化
+    // もし、env ファイル内に該当の設定がなければ、info を設定
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
     // ルーティング設定の作成
     // 第一引数で URL
     // 第二引数で、URL にマッチしたときに呼び出す関数を定義
     // 第二引数に渡す関数は、get(...) などでラップして HTTP メソッドを指定する
     // get(get_handler).post(post_handler) のように
     // メソッドチェーンで指定すれば複数のメソッドを指定できる
-    let app =  Router::new().route("/", get(root));
-    
+    let app = Router::new().route("/", get(root));
+
     // アドレスとポートの作成
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // ログにアドレスを表示
+    tracing::debug!("listening on {}", addr);
 
     // bind でアドレスとポートのバインディング（TCP コネクションの受け付け）
     // serve でサーバーを立ち上げ
@@ -26,7 +34,7 @@ async fn main() -> Result<()>{
     Ok(())
 }
 
-// GET "/" で返却する値を定義する関数 
+// GET "/" で返却する値を定義する関数
 async fn root() -> &'static str {
     "Hello, world!"
 }
