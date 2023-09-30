a9863ed3005289a42ea63a2b45d595f5e63e0f65
Flip451 <flipflap451@gmail.com>
2023-09-30 16:25:48 +0900

    feat: add labels table

---
+++ projects/hello_world_axum_2/migrations/20230930063708_label.sql
@@ -0,0 +1,16 @@
+-- labels テーブルを追加
+CREATE TABLE labels
+(
+    id          UUID PRIMARY KEY,
+    name        TEXT    NOT NULL
+);
+
+-- todos テーブルと labels テーブルを多対多対応させるためのテーブルを作成
+CREATE TABLE todo_labels
+(
+    todo_id     UUID    NOT NULL,
+    FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+    label_id    UUID    NOT NULL,
+    FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+    PRIMARY KEY (todo_id, label_id)
+);
