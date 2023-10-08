85e8c49a3611fa46810505e7835f5d519dd2f696
Flip451 <flipflap451@gmail.com>
2023-10-08 13:01:04 +0900

    feat: tests for user_get_all_application_service

--- projects/hello_world_axum_3/src/application/users/user_get_all_aplication_service.rs
+++ projects/hello_world_axum_3/src/application/users/user_get_all_aplication_service.rs
@@ -42,10 +42,62 @@ impl<T: IUserRepository> IUserGetAllApplicationService<T> for UserGetAllApplicat
 mod tests {
     use anyhow::Result;
 
+    use crate::{
+        domain::{
+            models::users::{User, UserName},
+            value_object::ValueObject,
+        },
+        infra::repository_impl::in_memory::users::in_memory_user_repository::InMemoryUserRepository,
+    };
+
     use super::*;
 
     #[tokio::test]
     async fn should_get_all_users() -> Result<()> {
-        Ok(todo!())
+        let repository = Arc::new(InMemoryUserRepository::new());
+
+        // 1. Get all stored user
+        let user_get_all_application_service =
+            UserGetAllApplicationService::new(repository.clone());
+        let command = UserGetAllCommand {};
+        let users = user_get_all_application_service.handle(command).await?;
+
+        assert!(users.is_empty());
+
+        // 2. Put the first data
+        let user_1 = User::new(UserName::new("tester-1".to_string())?)?;
+        let user_id = user_1.user_id().clone();
+        {
+            let mut store = repository.write_store_ref();
+            store.insert(user_id.clone(), user_1.clone());
+        }
+
+        // 3. Get all stored user
+        let command = UserGetAllCommand {};
+        let users = user_get_all_application_service.handle(command).await?;
+
+        assert_eq!(vec![UserData::new(user_1.clone())], users);
+
+        // 4. Put the second data
+        let user_2 = User::new(UserName::new("tester-2".to_string())?)?;
+        let user_id = user_2.user_id().clone();
+        {
+            let mut store = repository.write_store_ref();
+            store.insert(user_id.clone(), user_2.clone());
+        }
+
+        // 3. Get all stored user
+        let command = UserGetAllCommand {};
+        let mut users = user_get_all_application_service.handle(command).await?;
+
+        // Sort users alphabetically
+        users.sort_by(|a, b| a.user_name.cmp(&b.user_name));
+
+        assert_eq!(
+            vec![UserData::new(user_1), UserData::new(user_2)],
+            users
+        );
+
+        Ok(())
     }
 }
